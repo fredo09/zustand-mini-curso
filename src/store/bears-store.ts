52eb71fbaaae9331457d745276ea9fc3c0366f03
fredo09 <fred_15@outlook.com>
2024-07-07 22:57:00 -0600

    add new actions doNothing for state bears

--- src/store/bears-store.ts
+++ src/store/bears-store.ts
@@ -2,17 +2,26 @@
 * Declaracion del state usando zustand
 */
 
-import { create } from 'zustand'
+import { create } from 'zustand';
+
+interface Bear {
+    id: number,
+    name: string,
+}
 
 interface BearState {
     blackbears: number;
     pandaBears: number;
     polarBears: number;
 
+    bears: Bear[];
+
     //* Actions
     increase: (by: number) => void;
     increasePolar: (by: number) => void;
     increasePanda: (by: number) => void;
+
+    doNothing: () => void;
 }
 
 export const useBearStore = create<BearState>()((set) => ({
@@ -20,10 +29,15 @@ export const useBearStore = create<BearState>()((set) => ({
     pandaBears: 5,
     polarBears: 2,
 
+    bears: [ { id: 1, name: 'Bears # 1' } ],
+
 
     //* Acciones del tendra el estado para modificar su store
     increase: (by: number) => set((state) => ({ blackbears: state.blackbears + by })),
     increasePolar: (by: number) => set(state => ({ polarBears: state.polarBears + by })),
-    increasePanda: (by: number) => set((state) => ({ pandaBears : state.pandaBears + by }))
+    increasePanda: (by: number) => set((state) => ({ pandaBears : state.pandaBears + by })),
+
+
+    doNothing: () => set(( state ) => ({ bears: [...state.bears] }))
 
 }));
