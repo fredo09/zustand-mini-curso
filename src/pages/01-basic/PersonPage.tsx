86a3f76423abdcebc596d254818555af1000973d
fredo09 <fred_15@outlook.com>
2024-07-08 23:19:11 -0600

    add store person

--- src/pages/01-basic/PersonPage.tsx
+++ src/pages/01-basic/PersonPage.tsx
@@ -1,8 +1,17 @@
 import { WhiteCard } from '../../components';
-
+import { usePersonStore } from '../../store';
 
 
 export const PersonPage = () => {
+
+  //! Al usar zustand siempre sera de forma granular para obtener propiedades del store
+  const firstNameStore = usePersonStore( state => state.firstName );
+  const lastNameStore = usePersonStore( state => state.lastName );
+
+  // * Actions
+  const setFirstNameStore = usePersonStore( state => state.setFirstName );
+  const setLastNameStore = usePersonStore( state => state.setLastName );
+
   return (
     <>
       <h1>Persona</h1>
@@ -18,13 +27,14 @@ export const PersonPage = () => {
                   <label
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
-                    Primer Nombre
+                    Nombre
                   </label>
                   <input
                     type="text"
                     name="firstName"
                     id="firstName"
                     placeholder="Primer Nombre"
+                    onChange={(e) => setFirstNameStore(e.target.value)}
                   />
                 </div>
               </div>
@@ -40,6 +50,7 @@ export const PersonPage = () => {
                     name="lastName"
                     id="lastName"
                     placeholder="Apellido"
+                    onChange={(e) => setLastNameStore(e.target.value)}
                   />
                 </div>
               </div>
@@ -48,8 +59,8 @@ export const PersonPage = () => {
             <pre className="bg-gray-200 p-5 rounded-[20px]">
               {
                 JSON.stringify({
-                  firstName: '',
-                  lastName: ''
+                  firstName: firstNameStore,
+                  lastName: lastNameStore
                 }, null, 2)
               }
             </pre>
