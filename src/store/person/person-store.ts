df0c0d3de71af0df78e460a787cae27f24ff0885
fredo09 <fred_15@outlook.com>
2024-08-31 22:34:41 -0600

    add devtools redux

--- src/store/person/person-store.ts
+++ src/store/person/person-store.ts
@@ -2,7 +2,7 @@
  * Person state
  */
 import { type StateCreator, create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 //import { customSessionStore } from '../sessionStore/customSessionStore.store';
 import { firebaseSessionStore } from '../sessionStore/firebaseStore.stote';
 
@@ -23,25 +23,27 @@ interface ActonsState {
 }
 
 //! Separando el store con 
-const storeApi: StateCreator<PersonState & ActonsState> = (set) => ({
+const storeApi: StateCreator<PersonState & ActonsState, [["zustand/devtools", never]]> = (set) => ({
     //!Atributes
     firstName: '',
     lastName: '',
 
     //! ACTIONS
-    setFirstName: (value: string) => set(state => ({ firstName: value })),
-    setLastName: (value: string) => set(state => ({ lastName: value })),
+    setFirstName: (value: string) => set(({ firstName: value }), false, 'setFirstName' ),
+    setLastName: (value: string) => set(({ lastName: value }), false, 'setLastName'),
 });
 
 
 export const usePersonStore = create<PersonState & ActonsState>()(  
-    persist(
-        storeApi, //* -> Pongo mi declaracion de mi store aqui 
-        //! Parametro para la key donde se guardara en localStorage por defecto
-        {
-            name: 'person-store',
-            //storage: customSessionStore, //* -> Forma de crear el sesion store en zustand
-            storage: firebaseSessionStore, //* -> usando con firebase
-        }
+    devtools(
+        persist(
+            storeApi, //* -> Pongo mi declaracion de mi store aqui 
+            //! Parametro para la key donde se guardara en localStorage por defecto
+            {
+                name: 'person-store',
+                //storage: customSessionStore, //* -> Forma de crear el sesion store en zustand
+                storage: firebaseSessionStore, //* -> usando con firebase
+            }
+        )
     )
 );
