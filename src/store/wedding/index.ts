7316653784420135387a27600dbfa814cf0c83df
fredo09 <fred_15@outlook.com>
2025-01-10 17:49:37 -0600

    create a new slice and add new store

--- src/store/wedding/index.ts
+++ src/store/wedding/index.ts
@@ -1,9 +1,17 @@
 import { create } from "zustand";
 import { createPersonSlice, PersonSlice } from "./person-slice";
+import { createGuestSlice, GuestSlice } from './guest-slice';
+import { devtools } from "zustand/middleware";
+
+//* Combine slices
+type SharedSlice = PersonSlice & GuestSlice;
 
 //Create store
-export const useWeddingBoundStore = create<PersonSlice>()(
-    (...a) => ({
-        ...createPersonSlice(...a),
-    })
+export const useWeddingBoundStore = create<SharedSlice>()(
+    devtools(
+        (...a) => ({
+            ...createPersonSlice(...a),
+            ...createGuestSlice(...a),
+        })
+    )
 )
